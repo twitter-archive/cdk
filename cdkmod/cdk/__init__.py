b65ae4034c8dbedeef297cda330889f7543fe26c
Simeon Franklin <sfranklin@twitter.com>
2013-10-29 12:53:44 -0700

    Base features - deck creation and theme management are here

--- cdkmod/cdk/__init__.py
+++ cdkmod/cdk/__init__.py
@@ -8,62 +8,160 @@ Current features list includes:
  * single file output thanks to data:uri
 
 Usage:
-  cdk FILE
+  cdk [-vb] FILE
+  cdk --theme=<theme> FILE
+  cdk --install-theme=<theme>
+  cdk --default-theme=<theme>
 
 Arguments:
   FILE     asciidoc source file
+
 Options:
-  -h --help     Show this screen.
+  --install-theme <theme>      http path to zipfile containing theme. Will be unzipped in theme directory.
+  --default-theme <theme>      Theme to be the default used theme when creating slide decks
+  --theme <theme>              Theme to be used to create slide deck
+  -v --verbose                 Verbose output from underlying commands
+  -b --bare                    Simple html output, no slideshow.
+  -h --help                    Show this screen.
 
 """
 
-import subprocess
-from os.path import dirname, join, abspath
+import subprocess, zipfile
+from os.path import dirname, basename, join, abspath, isfile, isdir, expanduser
+from os import mkdir, unlink
+from shutil import copy
+import ConfigParser as cp
 
 from docopt import docopt
-from schema import Schema, And, Or, Use, SchemaError
 
-"""
-CDK_DIR=$( cd "$( dirname "$0" )" && pwd )
-eval ${CDK_DIR}/external/asciidoc/asciidoc.py -f ${CDK_DIR}/external/asciidoc/asciidoc.conf ${attrs[@]} $@
-"""
+LOCATION = abspath(dirname(__file__))
 
+PREFS_DIR = expanduser("~/.cdk")
+PREFS_FILE = join(PREFS_DIR, "prefs")
 
-def main():
-    """Entry point for stitching together our custom asciidoc conf, deckjs
-    conf and plugins.
-
+def set_default_theme(theme):
     """
-    data_dir = join(abspath(dirname(__file__)),  "data")
-    custom_dir = join(abspath(dirname(__file__)),  "custom")
-    asciidoc_dir = join(data_dir, "asciidoc-8.6.8")
-    
-    
-    # Setup asciidoc command we want to run
-    cmd = ("%(asciidoc_dir)s/asciidoc.py -b deckjs "
-           "-a deckjs_theme=twitter -a data-uri "
-           "-f %(custom_dir)s/asciidoc.conf "
-           "-a iconsdir='%(data_dir)s/images/icons' -a icons") % locals()
-    cmd = cmd.split()
-    
-    
-    # Try parsing command line args and flags with docopt
-    arguments = docopt(__doc__, version="cdk 0.1")
+    Set default theme name based in config file.
+    """
+    pref_init() # make sure config files exist
+    parser = cp.ConfigParser()
+    parser.read(PREFS_FILE)
+    # Do we need to create a section?
+    if not parser.has_section("theme"):
+        parser.add_section("theme")
+    parser.set("theme", "default", theme)
+    # best way to make sure no file truncation?
+    with open("%s.2" % PREFS_FILE, "w") as fp:
+        parser.write(fp)
+    copy("%s.2" % PREFS_FILE, PREFS_FILE)
+    unlink("%s.2" % PREFS_FILE,)
 
-    # Check for valid args and flags
-    schema = Schema({'FILE': And(str, len)})
+def pick_theme(manual):
+    """
+    Return theme name based on manual input, prefs file, or default to "plain"
+    """
+    if manual:
+        return manual
+    pref_init()
+    parser = cp.ConfigParser()
+    parser.read(PREFS_FILE)
     try:
-        arguments = schema.validate(arguments)
-    except SchemaError as e:
-        exit(e)
+        theme = parser.get("theme", "default")
+    except (cp.NoSectionError, cp.NoOptionError):
+        theme = "plain"
+    return theme
+    
+def pref_init():
+    """Can be called without penalty. Create ~/.cdk dir if it doesn't
+    exist. Copy the default pref file if it doesn't exist."""
+    
+    # make sure we have a ~/.cdk dir 
+    if not isdir(PREFS_DIR):
+        mkdir(PREFS_DIR)
+    # make sure we have a default prefs file
+    if not isfile(PREFS_FILE):
+        copy(join(LOCATION, "custom", "prefs"), PREFS_DIR)
+        
+def install_theme(path_to_theme):
+    """
+    Pass a path to a theme file which will be extracted to the themes directory.
+    """
+    pref_init()
+    # cp the file
+    filename = basename(path_to_theme)
+    themes = join(LOCATION, "custom", "deck.js", "themes")
+    dest = join(themes, filename)
+    copy(path_to_theme, dest)
+    # unzip
+    zf = zipfile.ZipFile(dest)
+    # should make sure zipfile contains only themename folder which doesn't conflict
+    # with existing themename. Or some kind of sanity check
+    zf.extractall(themes) # plus this is a potential security flaw pre 2.7.4
+    # remove the copied zipfile
+    unlink(dest)
 
-    # Great! Run asciidoc with appropriate flags
-    cmd.append(arguments['FILE'])
+def create_command(theme, bare=False, filters_list=None):
+    # default filters
+    if not filters_list:
+        filters_list = ["source/source-highlight-filter.conf",
+                        "graphviz/graphviz-filter.conf"]
+    # vars for locations
+    DATA_DIR = join(LOCATION,  "data")
+    CUSTOM_DIR = join(LOCATION,  "custom")
+    ASCIIDOC_DIR = join(DATA_DIR, "asciidoc-8.6.8")
+
+    # Setup asciidoc command we want to run with backend
+    if bare:
+        backend = "--conf-file=%(ASCIIDOC_DIR)s/html5.conf "
+    else:
+        backend = "--conf-file=%(CUSTOM_DIR)s/deckjs.conf "
+
+    filters = ["--conf-file=%(ASCIIDOC_DIR)s/filters/{}".format(f)
+                                            for f in filters_list]
+    filters = " ".join(filters)
+    
+    cmd = " ".join(["%(ASCIIDOC_DIR)s/asciidoc.py",
+                    "--no-conf --conf-file=%(CUSTOM_DIR)s/asciidoc.conf",
+                    backend,
+                    filters,
+                    "-b deckjs",
+                    "-a deckjs_theme=%(theme)s -a data-uri",
+                    "-a backend-confdir=%(CUSTOM_DIR)s",
+                    "-a iconsdir=%(DATA_DIR)s/images/icons -a icons"]) % locals()
+    return cmd.split()
+
+def run_command(cmd, args):
+    if args['--verbose']:
+        cmd.append('-v')
+        print "\n".join(cmd) + args['FILE']
+        
+    cmd.append(args['FILE'])
     try:
-        subprocess.check_output(cmd)
+        print subprocess.check_output(cmd)
     except subprocess.CalledProcessError as e:
         exit(e.output)
-        
-    
+
+def main():
+    """Entry point for choosing what subcommand to run.
+    """
+
+    # Try parsing command line args and flags with docopt
+    args = docopt(__doc__, version="cdk 0.1")
+    # Am I going to need validation? No Schema for the moment...
+    if args['FILE']:
+        # Great! Run asciidoc with appropriate flags
+        cmd = create_command(pick_theme(args['--theme']), args['--bare'])
+        run_command(cmd, args)
+    # other commands
+    elif args['--install-theme']:
+        path = args['--install-theme']
+        if not isfile(path):
+            exit("Theme file not found.")
+        if not path.endswith(".zip"):
+            exit("Theme installation currently only supports theme install from "
+                 ".zip files.")
+        install_theme(path)    
+    elif args['--default-theme']:
+        set_default_theme(args['--default-theme'])
 if __name__ == '__main__':
     main()
