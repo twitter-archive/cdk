b2d4094c84152cd63af674e76458087991eb9a23
Simeon Franklin <sfranklin@twitter.com>
2014-01-13 09:23:48 -0800

    Push out a point release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE') as f:
 
 setup(
     name='cdk',
-    version='1.0.2',
+    version='1.0.3',
     description='Courseware Developement Kit based on asciidoc and deck.js',
     long_description=readme,
     author='Simeon Franklin',
