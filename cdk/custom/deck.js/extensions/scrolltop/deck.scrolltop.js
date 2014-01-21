4f00f738314ebfd07e0ebe1382ceb45eff208aa5
Simeon Franklin <sfranklin@twitter.com>
2014-01-21 13:18:43 -0800

    Scrolltop plugin should also set overflow property so small slides don't have scrollbars

--- cdk/custom/deck.js/extensions/scrolltop/deck.scrolltop.js
+++ cdk/custom/deck.js/extensions/scrolltop/deck.scrolltop.js
@@ -1,13 +1,10 @@
-/*!
-
-Copyright (c) 2012 Simeon Franklin
-MIT license
-*/
-
 /*
 
   This module makes sure that switching to a new slide always scrolls
   to the top.
+
+  It also checks body/slide height - for tall slides set overflow to scroll,
+  for short slides hide scrollbars
 
   Currently in deck.js presentations with long slides, switching from
   one slide scrolled down the page to the next slide doesn't change
@@ -22,6 +19,21 @@ MIT license
        var scrollup = function(evt, from, to){
            $(document).scrollTop(0);
        };
+       var $body = $('body');
+       var $html = $('html');    
+       var height_check = function(evt, from, to){
+           if(to > 0){
+               var slide = $('#slide-' + to);
+           }else{
+               var slide = $('#title-slide');
+           }
+           if(slide.height() > $body.height()){
+               $html.css('overflow', 'scroll');
+           }else{
+               $html.css('overflow', 'hidden');
+           }
+       }
        $d.bind('deck.change', scrollup);
+       $d.bind('deck.change', height_check); 
     })
 })(jQuery, 'deck');
