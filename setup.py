8aec2b77c949b9586647558608920cc24cbf2b76
Simeon Franklin <sfranklin@twitter.com>
2014-01-22 13:55:29 -0800

    Bumping version number for release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE') as f:
 
 setup(
     name='cdk',
-    version='1.0.4',
+    version='1.0.5',
     description='Courseware Developement Kit based on asciidoc and deck.js',
     long_description=readme,
     author='Simeon Franklin',
